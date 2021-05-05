36f644e61619f6f667c802ee1bcf87b7d1dc50a5
numantariq435 <numantariq435@gmail.com>
2021-05-05 15:56:47 +0500

    update

---
+++ closures/index.js
@@ -0,0 +1,15 @@
+function outerFunction() {
+    console.log('OuterFunction');
+
+    return function innerFunction() {
+        console.log('InnerFunction');
+
+        return function innerInnerFunction() {
+            console.log('InnerInnerFunction');
+        }
+    }
+}
+
+outerFunction(); // run only outerFunction
+outerFunction()(); // run outerFunction + innerFunction
+outerFunction()()(); // run outerFunction + innerFunction  + innerInnerFunction
